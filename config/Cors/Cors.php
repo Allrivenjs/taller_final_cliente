406f5204bf28eeadb738a0c2c39dab053e3164ed
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-01-22 11:14:11 -0500

    Update Cors.php

--- config/Cors/Cors.php
+++ config/Cors/Cors.php
@@ -11,9 +11,11 @@ final class Cors
         if (isset($_SERVER['HTTP_ORIGIN'])) {
             // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
             // you want to allow, and if so:
-            header("Access-Control-Allow-Origin: *");
-            header('Access-Control-Allow-Credentials: true');
-            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+            header('Access-Control-Allow-Origin: *');
+            header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+            header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+//            header('Access-Control-Max-Age: 86400');    // cache for 1 day
         }
 
         // Access-Control headers are received during OPTIONS requests
